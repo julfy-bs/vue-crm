352c54fa4b1dc19c8a31831e7dc460cbad4a1de3
julfy-bs <julfy.web@gmail.com>
2020-11-09 18:45:08 +0300

    switch to dev branch

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-main-layout">
+    <Navbar @burgerClick="isOpen = !isOpen" />
+    <Sidebar v-model="isOpen" />
+
+    <main class="app-content" :class="{ full: !isOpen }">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
+
+export default {
+  name: "main-layout",
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    Sidebar,
+    Navbar
+  }
+};
+</script>
